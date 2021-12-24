54ca5df7af8fcd81c37cbba29e57452573995b58
plastic-jellyfish <kark027@gmail.com>
2021-12-24 21:20:21 +0530

    2game Menu

--- sketch.js
+++ sketch.js
@@ -9,43 +9,90 @@ let totalTime = 1000;
 let _width, _height;
 let a=0 ,b=0;
 let s=0;
+let _DroneFlag = 0;
+let _WiredFlag = 0;
+let _peopleYellow = [];
+let _power = [];
+let sc=0;
+let numYellow =10;
+// let savedTime = 0;
+let savedTime1 = 0;
+let activeYellow;
+// let totalTime = 1000;
+let numPower =1;
+let activePower=0;
+let font, font1;
+
 
 function preload() {
     font = loadFont('assets/ZillaSlab-Regular.ttf');
-    _width = select(".container3").width;
-    _height = select(".container3").height;
+    font1 = loadFont('assets/Kanit-Light.ttf');
+    _width = select(".container2").width;
+    _height = select(".container2").height;
 }
 
 function setup() {
   var drone = createCanvas(_width,_height);  
   drone.parent('drone');
-  background(0);
-  dronex = width/2;
-  droney = 0.15 * height;
-  dronew = 0.1 * width;
-  droneh = 0.2 * height;
-  dspeed=1;
-  shoot=50;
-  restart();
-  savedTime = millis();
-  active=20;
-  ex=0;
-  k=0.05;
-  s = width;
-  textFont(font);
-  textSize(12);
-  fear = 0;
-  rectMode(CORNERS);
 }
 
 function windowResized() {
-    _width = select(".container3").width;
-    _height = select(".container3").height;
+    _width = select(".container2").width;
+    _height = select(".container2").height;
     resizeCanvas(_width, _height);
-    restart();
+    // restart();
 }
 
 function draw() {
+  if((_DroneFlag == 0) && (_WiredFlag == 0)) _Menu();
+  if(_DroneFlag == 1) _Drone();
+  if(_WiredFlag == 1) _Wired();
+}
+//******************************************************************************************//
+//***********************************MENU************************************************//
+//******************************************************************************************//
+
+function _Menu(){
+  let menuPanel = createDiv();
+  menuPanel.style('background','#000')
+  menuPanel.size(_width, 1.4*_height)
+  menuPanel.position(0,0)
+     
+  let droneButton = createButton('Feeling Dull...');
+  droneButton.style('background','#fff')
+  droneButton.style('font-size','20px')
+  droneButton.style('border','0')
+  droneButton.style('padding','25px')
+  droneButton.position(_width/2-120, _height/2+50)
+  droneButton.style('width','200px')
+  droneButton.style('box-shadow', '10px 10px #000')
+  droneButton.mousePressed(() => {
+    restartDrone();
+    _Drone();
+  })
+
+  let unwireButton = createButton('Feeling Anarchy');
+  unwireButton.style('background','#fff')
+  unwireButton.style('font-size','20px')
+  unwireButton.style('border','0')
+  unwireButton.style('padding','25px')
+  unwireButton.position(_width/2-120, _height/2+200)
+  unwireButton.style('width','200px')
+  unwireButton.style('box-shadow', '10px 10px #000')
+  unwireButton.mousePressed(() => {
+    restartWired();
+    _Wired();
+  })
+}
+
+//******************************************************************************************//
+//***********************************DRONE************************************************//
+//******************************************************************************************//
+
+function _Drone() {
+  removeElements()
+  console.log('DRONE_ENTERED')
+  _DroneFlag = 1;
   background(10);
   buildings();
   let eye = map(dronex,0,width, width/2 - 50, width/2 + 50);
@@ -97,15 +144,47 @@ function draw() {
   }
 
   if (fear>=100) {
-    let reset = createP('Good Job! You killed '+devkill+' Deviants & '+civilian+' Civilians today.\n Continue working Next Shift?');
-    reset.style('background','#02458880')
+    let reset = createP('Dull Day & Fine Job! You killed '+devkill+' Deviants & '+civilian+' Civilians today.\n Continue working Next Shift?');
+    reset.style('background','#0050ff')
     reset.style('font-size','20px')
     reset.style('border','0')
     reset.style('padding','25px')
     reset.position(_width/2-120, _height/2+50)
     reset.style('width','200px')
+    reset.style('height','400px')
     reset.style('box-shadow', '10px 10px #000')
-    reset.mousePressed(restart)
+    reset.mousePressed(() => {
+      restartDrone();
+      _Drone();
+    })
+    let yes = createButton('YES');
+    yes.style('background','#fff')
+    yes.style('font-size','20px')
+    yes.style('border','0')
+    yes.style('padding','25px')
+    yes.position(_width/2-50, _height/2+250)
+    yes.style('width','100px')
+    // yes.style('height','400px')
+    yes.style('box-shadow', '2px 2px #000')
+    yes.mousePressed(() => {
+      restartDrone();
+      _Drone();
+    })
+
+    let no = createButton('NO');
+    no.style('background','#fff')
+    no.style('font-size','20px')
+    no.style('border','0')
+    no.style('padding','25px')
+    no.position(_width/2-50, _height/2+350)
+    no.style('width','100px')
+    // yes.style('height','400px')
+    no.style('box-shadow', '2px 2px #000')
+    no.mousePressed(() => {
+      removeElements()
+      _DroneFlag =0;
+    })
+
   }
 
   fill(0);
@@ -127,12 +206,26 @@ function draw() {
   rect((width/2)-11, droney-32, (width/2)-11+105, droney-30+11,5);
 }
 
-function restart() {
+function restartDrone() {
   removeElements();
-  fear = 0;
   devkill =0;
   civilian=0;
-  active = 20;
+  dronex = width/2;
+  droney = 0.15 * height;
+  dronew = 0.1 * width;
+  droneh = 0.2 * height;
+  dspeed=1;
+  shoot=50;
+  savedTime = millis();
+  active=20;
+  ex=0;
+  k=0.05;
+  s = width;
+  textFont(font);
+  textSize(12);
+  fear = 98;
+  rectMode(CORNERS);
+
   // _people = new People[numPeople];
   for (let i=0; i<numPeople; i++) {
     newpeople = new People(i);
@@ -162,8 +255,6 @@ function buildings(){
   s -= 1;
   if(s<-200) s=width+200;
 }
-
-
 
 class People {
   // float head, body, x, speed, limb;
@@ -267,30 +358,7 @@ function drone() {
         dronex=width;
         dspeed *= -1;
     }
-    // select(".Left").mouseClicked(()=>{
-    //     if ((dronex>= 0)&&(dronex<=width)) {
-    //         dronex=dronex-.05*dspeed;
-    //         if (dronex<0) {
-    //           dronex=0;
-    //         }
-    //     }
-    //     if (dronex>(width-200)) {
-    //           dronex=dronex-.05*dspeed;
-    //     }
-    // })
-
-    // select(".Right").mouseClicked(()=>{
-    //     if ((dronex>= 0)&&(dronex<=width)) {
-    //         dronex=dronex+.05*dspeed;
-    //         if (dronex>width) {
-    //           dronex=width;
-    //         }
-    //     }
-    //     if (dronex>(width-200)) {
-    //           ex=ex-.05*dspeed;
-    //     }
-    // })
-
+ 
     select(".Shoot").touchStarted(()=>{
       shoot=255;
       for (let i=0; i<active ; i++) {
@@ -338,4 +406,360 @@ function drone() {
             shoot=50;
           }
     })
+}
+
+//******************************************************************************************//
+//***********************************UNWIRED************************************************//
+//******************************************************************************************//
+
+function _Wired() {
+  removeElements()
+  let topPanel = createDiv();
+  topPanel.style('background','#555')
+  topPanel.size(_width, .215*_height)
+  topPanel.position(0,0)
+  
+  let bottomPanel = createDiv();
+  bottomPanel.style('background','#555')
+  bottomPanel.size(_width, .21*_height)
+  bottomPanel.position(0,1.215*_height)
+     
+  let wireText = createP('The Reds have kept the Yellows wired to thier ideals. Its time to Un-Wire them.'+
+  'You are the blue, interact & organise 4 yellows and destruct all the Reds before the time runs out.'+ 
+  'All the Best!');
+  wireText.position(_width/2 - 100, 2)
+  wireText.style('width','300px')
+  wireText.style('font',font1)
+
+  let title = createDiv('UN-WIRE');
+  title.style('background','#fff')
+  title.style('font-size','20px')
+  title.position(10, 50)
+  title.style('width','90px')
+  title.style('padding','5px')
+  title.style('box-shadow', '5px 5px #000')
+  
+  background(150);
+  _WiredFlag = 1;
+  if (activePower>0) {
+    for (let j =0 ; j<numPower;j++) {
+      _power[j].display();
+      _power[j].attack();
+      if (_power[j].plife==1) {
+        _power[j].scan();
+      }
+      _power[j].recover();
+    }
+
+    for (let i =0 ; i<numYellow;i++) {
+      _peopleYellow[i].display();
+      _peopleYellow[i].move();
+    }
+    fill(0, 150);
+    noStroke();
+    rect(mouseX, mouseY, 40, 15, 10);
+    stroke(250);
+    fill(0, 0, 255);
+    ellipse(mouseX, mouseY, 20, 20);
+  }
+  else {
+    for (let j =0 ; j<numPower;j++) {
+      _power[j].display();
+    }
+    for (let i =0 ; i<numYellow;i++) {
+      _peopleYellow[i].display();
+    }
+    fill(255, 150);
+    noStroke();
+    rect(mouseX, mouseY, 40, 15, 10);
+    stroke(250);
+    fill(0, 0, 255);
+    ellipse(mouseX, mouseY, 20, 20);
+
+    let reset = createP('Good Job! The Reds are destructed and you have unwired all them. Un-wire more?');
+    reset.style('background','#0050ff')
+    reset.style('font-size','20px')
+    reset.style('border','0')
+    reset.style('padding','25px')
+    reset.position(_width/2-120, _height/2+50)
+    reset.style('width','200px')
+    reset.style('height','400px')
+    reset.style('box-shadow', '10px 10px #000')
+    reset.mousePressed(() => {
+      restartWired();
+      _Wired();
+    })
+    let yes = createButton('YES');
+    yes.style('background','#fff')
+    yes.style('font-size','20px')
+    yes.style('border','0')
+    yes.style('padding','25px')
+    yes.position(_width/2-50, _height/2+250)
+    yes.style('width','100px')
+    // yes.style('height','400px')
+    yes.style('box-shadow', '2px 2px #000')
+    yes.mousePressed(() => {
+      restartWired();
+      _Wired();
+    })
+
+    let no = createButton('NO');
+    no.style('background','#fff')
+    no.style('font-size','20px')
+    no.style('border','0')
+    no.style('padding','25px')
+    no.position(_width/2-50, _height/2+350)
+    no.style('width','100px')
+    // yes.style('height','400px')
+    no.style('box-shadow', '2px 2px #000')
+    no.mousePressed(() => {
+      removeElements()
+      _WiredFlag =0;
+    })
+
+
+  }
+}
+
+function restartWired() {
+  removeElements();
+  activeYellow= 0;
+  noCursor();
+  rectMode(CENTER);
+  textFont(font1);
+  textSize(12);
+  textAlign(CENTER,CENTER);
+
+  for (let i=0; i<numYellow; i++) {
+    newpeople = new PeopleYellow(i);
+    _peopleYellow[i] = newpeople;
+  }
+  for (let j=0; j<numPower; j++) {
+    newpower = new Power(j);
+    _power[j] = newpower;
+  }
+  activePower=numPower;
+}
+
+class PeopleYellow {
+  // let x, y, speedx, speedy, speedwx, speedwy;
+  // let wired, life;
+
+  constructor(i) {
+    this.life = 1;
+    this.wired =1;
+    this.x = random(100, _width-100);
+    this.y = random(100, _height-100);
+    this.speedx = random(-0.5, 0.5);
+    this.speedy = random(-0.5, 0.5);
+    if (this.speedx>0) {
+      this.speedx = this.speedx+ 0.5;
+    }
+    else { 
+      this.speedx = this.speedx- 0.5;
+    }
+    // console.log('people:'+i+':'+this.life)
+  }
+
+  move() {
+    if ((this.wired==1) ||((this.wired==0)&&(this.activePower==0))) {
+      if ((this.x>(mouseX-10))&&(this.x<(mouseX+10))&&(this.y>(mouseY-10))&&(this.y<(mouseY+10))) {
+        this.talk();
+        let passedTime = millis() - savedTime;
+        // console.log(millis(), passedTime, savedTime)
+
+        if (passedTime > (2* totalTime)) {
+          //          activeYellow=activeYellow+1;
+          this.wired=0;
+          savedTime = millis(); // Save the current time to restart the timer!
+          // console.log(savedTime)
+        }
+      }
+      else {
+        if ((this.x<0)||(this.x>width)) {
+          this.speedx=this.speedx*-1;
+        }
+        if ((this.y<0)||(this.y>height)) {
+          this.speedy=this.speedy*-1;
+        }
+        this.x=this.x+this.speedx;
+        this.y=this.y+this.speedy;
+      }
+    }
+    if ((this.wired==0) &&(activePower>0)) {
+      let j=0;
+      for (let i=(numPower-1); i>=0; i--) {
+        if (_power[i].plife==1) {
+          j=i;
+        }
+      }
+
+      this.speedwx=(_power[j].px-this.x)/100;
+      this.speedwy=(_power[j].py-this.y)/100;
+      this.x=this.x+this.speedwx;
+      this.y=this.y+this.speedwy;
+    }
+  }
+
+  talk() {
+    fill(255);
+    noStroke();
+    textSize(14);
+    // textAlign(RIGHT);
+    text("#@*%", this.x+30, mouseY-20);
+  }
+
+  display() {
+    if (this.life==1) {
+      fill(0, 150);
+      noStroke();
+      rect(this.x, this.y, 40, 15, 10);
+
+      if (this.wired==1) {
+        fill(255, 255, 0, 200);
+        stroke(100);
+      }
+      else if (this.wired==0) {
+        fill(255, 200);
+        stroke(255);
+      }
+
+      strokeWeight(2);
+      ellipse(this.x, this.y, 20, 20);
+    }
+  }
+}
+
+class Power {
+//   let px, py;
+//   let plife, strength, recoverTime;
+
+  constructor(i) {
+    this.plife = 1;
+    if (i==0) {
+      this.px=(_width/4);
+      this.py=(_height/4);
+    }
+    if (i==1) {
+      this.px=(_width/4)*3;
+      this.py= (_height/4);
+    }
+    if (i==2) {
+      this.px=(_width/2);
+      this.py= (_height/2);
+    }
+    if (i==3) {
+      this.px=(_width/4);
+      this.py= (_height/4)*3;
+    }
+    if (i==4) {
+      this.px=(_width/4)*3;
+      this.py= (_height/4)*3;
+    }
+    this.strength=4;
+    this.recoverTime=0;
+    // console.log('power:'+i+':'+this.plife)
+  }
+
+  scan()
+  {
+    let passedTime1 = millis() - savedTime1;
+    if (passedTime1 > (1*totalTime)) {
+
+      if ((_peopleYellow[sc].wired)==0) {
+        _peopleYellow[sc].wired=1;
+      }
+
+      sc=sc+1;
+      if (sc==numYellow) {
+        sc=0;
+      }
+      savedTime1 = millis(); // Save the current time to restart the timer!
+    }
+  }
+
+  attack() {
+    for (let i=0;i<numPower;i++) {
+      for (let j =0;j<numYellow;j++) {
+        if ((_peopleYellow[j].wired==0)&&(_power[i].plife==1)) {
+
+          if ((_peopleYellow[j].x>=(_power[i].px-20)) && (_peopleYellow[j].x<=(_power[i].px+20))&&(_peopleYellow[j].y>=(_power[i].py-20)) && (_peopleYellow[j].y<=(_power[i].py+20))) {
+            //              print(" x:"+_peopleYellow[j].x+" px:"+_power[i].px+" y:"+_peopleYellow[j].y+" py:"+_power[i].py);
+            activeYellow=activeYellow+1;
+            if (activeYellow==1) {
+              _power[i].strength=3;
+            }
+            else if (activeYellow==2) {
+              _power[i].strength=2;
+            }
+            else if (activeYellow==3) {
+              _power[i].strength=1;
+            }
+            //           print(" Pow:"+i+" Strength:"+_power[i].strength+" activeYellow:"+activeYellow);
+          }
+          if (activeYellow>=4) {
+            _power[i].plife=0;
+            _power[i].strength=0;
+            _power[i].recoverTime = millis();
+            for (let k =0;k<numYellow;k++) {
+              if (activePower>1) {
+                _peopleYellow[k].wired=1;
+              }
+            }
+            activeYellow=0;
+            activePower=activePower-1;
+          }
+        }
+      }
+      activeYellow=0;
+    }
+  }
+
+  recover() {
+    if (this.plife==0) {
+      let passedTime2 = millis() - this.recoverTime;
+      fill(0);
+      noStroke();
+      textSize(18);
+      textAlign(CENTER,CENTER);
+      text(int(((120*totalTime)/1000)-(passedTime2/1000)), this.px, this.py);
+      if (passedTime2 > (120*totalTime)) {
+        this.plife=1;
+        this.strength=4;
+        this.activePower=this.activePower+1;
+      }
+    }
+  }
+  display() {
+    if (this.plife==1) {
+      strokeWeight(5);
+      stroke(255, 255, 0, 50);    
+      line(this.px, this.py, _peopleYellow[sc].x, _peopleYellow[sc].y);
+      stroke(50, 10);
+      for (let i=0; i<numYellow;i++) {
+        if ((i!=sc)&&(_peopleYellow[i].wired==1)) {
+          line(this.px, this.py, _peopleYellow[i].x, _peopleYellow[i].y);
+        }
+      }
+    }
+
+    if ((this.plife==1)&&(this.strength==4)) {
+      fill(255, 0, 0);
+    }
+    else if ((this.plife==1)&&(this.strength==3)) {
+      fill(255, 128, 0);
+    }
+    else if ((this.plife==1)&&(this.strength==2)) {
+      fill(255, 200, 0);
+    }
+    else if ((this.plife==1)&&(this.strength==1)) {
+      fill(255, 250, 0);
+    }
+    else if (this.plife==0) {
+      fill(150);
+    }
+    stroke(100);
+    strokeWeight(3);
+    ellipse(this.px, this.py, 50, 50);
+  }
 }
