6bee5824c1848be8ddb3c1653eff10497e37f320
plastic-jellyfish <kark027@gmail.com>
2021-12-29 12:58:13 +0530

    highscorecheck

--- server.js
+++ server.js
@@ -9,18 +9,14 @@ const gameName = process.env.GAMENAME;
 let url = 'https://dull-and-disorder.herokuapp.com/';
 // let url = 'http://localhost:5000/'
 const app = express()
-const log4js = require("log4js");
-log4js.configure({
-  appenders: { cheese: { type: "file", filename: "cheese.log" } },
-  categories: { default: { appenders: ["cheese"], level: "error" } }
-});
-const logger = log4js.getLogger("cheese");
 
 let userID = undefined;
+let userName = undefined;
 let inlineID = undefined;
 let msgID = undefined;
 let chatID = undefined;
 let score = 0;
+let highScoreCurrUser = 0;
 
 const markup = Markup.inlineKeyboard([
   Markup.button.game('🎮 Play now!'),
@@ -38,8 +34,9 @@ app.get('/', (req,res) => {
 bot.start((ctx) => ctx.replyWithGame(gameName))
 bot.command('foo', (ctx) => ctx.replyWithGame(gameName, markup))
 bot.command('help', (ctx) => ctx.reply('This is Game Bot!'))
-bot.gameQuery((ctx) => {
+bot.gameQuery(async (ctx) => {
   userID = ctx.callbackQuery.from.id;
+  userName = ctx.callbackQuery.from.first_name;
   inlineID = ctx.callbackQuery.inline_message_id;
   if(ctx.callbackQuery.message){
     msgID = ctx.callbackQuery.message.message_id
@@ -50,42 +47,37 @@ bot.gameQuery((ctx) => {
     chatID=undefined
   }
   console.log('user_id:'+userID+', inline_id:'+inlineID+', message_id:'+msgID+ ', chat_id:'+chatID );
-  logger.error('user_id:'+userID+', inline_id:'+inlineID+', message_id:'+msgID+ ', chat_id:'+chatID );
   ctx.answerGameQuery(url)
+  await highScore();
+  console.log("HighScore of "+ userName+":" + highScoreCurrUser)
 })
 
 app.put('/score', async (req,res) => {
   score = req.body.score
-  try{
-    await bot.telegram.setGameScore(userID,score,inlineID, chatID, msgID); 
-  } catch(e){
-    if(e.response.error_code === 400){
-      console.log("Error_code 400 Score not modified")
-    } else {
-      console.log("Unknown error",e)
+  if(score > highScoreCurrUser){
+    try{
+      await bot.telegram.setGameScore(userID,score,inlineID, chatID, msgID); 
+      console.log("Current_Score:"+ score +" > previous high score:"+highScoreCurrUser+" New Score updated.")
+    } catch(e){
+      if(e.response.description === 'Bad Request: BOT_SCORE_NOT_MODIFIED'){
+        console.log("Error_code 400::Current Score less than High Score::Bad Request: BOT_SCORE_NOT_MODIFIED")
+      } else {
+        console.log("Unknown error",e)
+      }
     }
-    logger.error("Cheese is too ripe! Set Score Error",e);
-  }
-  // bot.telegram.sendMessage(userID, "Score:"+ score)
-  console.log(score)
-  logger.error("Cheese is Comté."+score);
+  } else {console.log("Current Score:"+ score +" < previous high score:"+highScoreCurrUser+" New Score NOT updated.");}
 })
 
-// bot.command('score', async (ctx) => {
-//   ctx.reply('HighScoreTable:')
-//   let highScore = await bot.telegram.getGameHighScores(userID,inlineID, chatID, msgID);
-//   console.log(highScore.position)
-//   console.log(highScore.score)
-//   // ctx.reply("HighScoreTable:"+"position:"+ highScore.position + "user:"+ highScore.user.first_name + "score:" + highScore.score)
-// });
-
-
-// logger.trace("Entering cheese testing");
-// logger.debug("Got cheese.");
-// logger.info("Cheese is Comté.");
-// logger.warn("Cheese is quite smelly.");
-// logger.error("Cheese is too ripe!");
-// logger.fatal("Cheese was breeding ground for listeria.");
+async function highScore(){
+  try{
+    let higher = await bot.telegram.getGameHighScores(userID,inlineID, chatID, msgID);
+    for(let i=0; i < higher.length; i++){
+      if(higher[i].user.id === userID) {highScoreCurrUser = higher[i].score;}
+    }
+  } catch(e){
+    console.log("Error while requesting Highscore Table", e)
+  }
+}
 
 bot.launch()
 app.listen(process.env.PORT || 5000)
